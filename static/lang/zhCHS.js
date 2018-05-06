2562cfa4b1188816ace214703a7da51f4c4729e4
nienzu <ibqqz0602@gmail.com>
2018-05-06 11:25:46 +0800

    Public web

---
+++ static/lang/zhCHS.js
@@ -0,0 +1,120 @@
+module.exports={
+  Nav:{
+    home:'首页',
+    apply:'报名',
+    agenda:'议程',
+    news:'动态',
+    thanks:'感谢',
+    aboutUs:'关于我们',
+    community:'社区',
+    lang:'语言',
+  },
+  AboutUs:{
+    about:'工作人员名单',
+    about1:'总召',
+    about2:'设计组',
+    about3:'导演组',
+    about4:'开发组',
+    about5:'行政组',
+    contactWay:'联系方式',
+    footer1:'畅想未来，拥抱开源',
+    contact:'联系我们: soscon@openingsource.org',
+    footer:'学生开源年会',
+    email:'官方邮箱',
+    email1:'总召',
+    email2:'行政组',
+    email3:"设计组",
+    email4:'导演组',
+    email5:'开发组',
+   /* contact1:'总召：刘煜民',
+    contact2:'导演组：叶伟豪',
+    contact3:'设计组：蒋昊辰',
+    contact4:'开发组：邵明琦',
+    contact5:'志愿者团队：刘昊天',
+    attend:'关注我们',
+    foot:'畅想未来，拥抱开源。Copyleft@2018学生开源年会 SOSCON.top'*/
+  },
+  Community:{
+    community:'联合创始社区',
+    community1:'合作社区',
+    media:'合作媒体',
+    foot:'畅想未来，拥抱开源',
+    contactus:'联系我们: soscon@openingsource.org',
+    foot1:'学生开源年会',
+  },
+  About:{
+    aboutp1:'GNOME 基金会亚洲委员会在 2008 年成立，旨在通过举办 GNOME.Asia 亚洲峰会在亚洲地区推广GNOME项目以及自由开源软体的理念。GNOME.Asia 峰会与 GUADEC 峰会和 Boston 峰会一起成为了 GNOME 基金会每年的全球三大活动。此前的 GNOME.Asia 分别在北京、越南胡志明、台北、印度班加罗尔、香港和韩国首尔、印尼雅加达和重庆举办。',
+
+    hoping:'年会展望',
+    hopingp1:'SOSCON 2018 将于 2018年 6 月9日-10日在火热的重庆举办，来自全国各地的学生届时将齐聚一堂，想必是一场技术与思考的饕餮盛宴！',
+    hopingp2:'我们也希望SOSCON所聚集而成的社群会作为一个长期的舞台，活跃在各地，成为学生交流技术、开放讨论的平台，使得开源理念能够在学生群体中茁壮成长。',
+    hopingp3:'会议期间希望学生能积极交流，让学生站上舞台，展示诞生在实验室里的创想，分享蕴含在编码中的激情，畅聊属于开源理念的未来。也期待参会者能继续发挥自己的影响力，让周围更多的人为开源做贡献。',
+
+    time:'举办时间：',
+    time1:'2018年8月11.12日（Sat & Sun)',
+
+    place:'地点：',
+    place1:'台湾科技大学',
+
+    bottom1:'畅想未来，拥抱开源',
+    bottom2:'联系我们: soscon@openingsource.org',
+
+    map:"查看更大的地图"
+  },
+  Apply:{
+    cfp:"招募讲者",
+    summit:"报名",
+    info:"重要讯息",
+    deadline:"投稿期限：",
+    deadline1:"5 月 25 日",
+    notification:"投稿公布时间：",
+    notification1:"2018-06-01 until 2018-06-05",
+
+    agenda:"日程表预定公布时间：",
+    agenda1:"七月上旬",
+    date:'活动时间：',
+    date1:'8 月 11 - 12 日',
+    location:'举办地点：',
+    location1:'台湾科技大学',
+    
+    applyInfo1:"与 GNOME 有关的开发、本地化、社区运营、推广等话题，包括但不限于：",
+    list0:"对 GNOME 贡献",
+    list1: "GNOME 的最新开发动态",
+    list2: "开发 GNOME 3 的应用",
+    list3: "GNOME 界面设计",
+    list4: "GNOME 测试",
+    list5: "GNOME 辅助功能",
+    list6: "GNOME人机界面工程（图标和图形设计）",
+    list7: "GNOME 市场",
+    list8: "亚洲成功案例或本地 GNOME 项目",
+    list9: "GNOME 和教育",
+    list10: "GNOME 女性拓展计划",
+    list11: "在移动设备上使用和开发 GNOME (智能手机和平板)",
+    list12: "在嵌入设备或开源硬体上使用和开发 GNOME",
+
+
+
+    linux:"Contributing to Linux and F/LOSS",
+
+    olist1:"Linux 内核及开发",
+    olist2:"开源操作系统的开发、使用和技术、推广",
+    olist3:"Debian、Fedora、openSUSE、Ubuntu、及 FreeBSD 等发行版相关的话题",
+    olist4:"其他开源项目有关的开发、使用、推广话题",
+    
+    story:"Open Source Enlightenment and Related Story",
+
+    olist5:"你的开源故事、如何参与开源社区、开源经历",
+    olist6:"女生如何更好地参与开源",
+    olist7:"在生活及工作中如何使用开源工具改善流程与提高效率",
+
+    emerging:"Emerging Technologies",
+    
+    olist8:"人工智慧、深度学习、机器学习相关话题",
+    olist9:"AR/VR 虚拟显示、增强显示、显示技术",
+    olist10:"大数据",
+    olist11:"分散式系统",
+
+  }
+}
+
+
